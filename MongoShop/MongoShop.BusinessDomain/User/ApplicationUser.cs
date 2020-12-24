95d087a1c1b7bf35af78baae9827e5b58347b9d4
QuyenTho <QuyenTho>
2020-12-24 10:14:46 +0700

    Remove redundant properties.

--- MongoShop/MongoShop.BusinessDomain/User/ApplicationUser.cs
+++ MongoShop/MongoShop.BusinessDomain/User/ApplicationUser.cs
@@ -24,32 +24,14 @@ namespace MongoShop.BusinessDomain.User
         [BsonElement("name")]
         public string Name { get; set; }
 
-        [BsonElement("permission")]
-        public string Permission { get; set; }
-
-        [BsonElement("password")]
-        public string Password { get; set; }
-
         [BsonElement("status")]
         public bool Status { get; set; }
-
-        [BsonElement("external_login")]
-        public List<ExternalLogin> ExternalLogin { get; set; }
 
         [BsonElement("contacts")]
         public List<Contacts> Contacts { get; set; }
 
         [BsonElement("cart")]
         public Cart Cart { get; set; }
-
-        [BsonElement("reset_password_token")]
-        public string ResetPasswordToken { get; set; }
-
-        [BsonIgnore]
-        public string AuthenticationType { get; set; }
-
-        [BsonIgnore]
-        public bool IsAuthenticated { get; set; }
     }
 
     public class Cart
