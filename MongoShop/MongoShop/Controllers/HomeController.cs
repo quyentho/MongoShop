143c768d541e4dbdea3da0897627f1c9ff7b937f
QuyenTho <QuyenTho>
2020-12-24 11:59:21 +0700

    User adminLTE template for admin area.

--- MongoShop/MongoShop/Controllers/HomeController.cs
+++ MongoShop/MongoShop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,6 +13,7 @@ using MongoShop.Models;
 
 namespace MongoShop.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly IProductServices _productServices;
