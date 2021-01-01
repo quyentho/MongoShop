70de79ac6748613ddbd80b3fc1d9da2151500a01
QuyenTho <QuyenTho>
2021-01-01 16:03:18 +0700

    Get rid of ImagesUpload model.

--- MongoShop/MongoShop/Services/FileUpload/IFileUploadService.cs
+++ MongoShop/MongoShop/Services/FileUpload/IFileUploadService.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace MongoShop.Services.FileUpload
 {
     public interface IFileUploadService
     {
-        Task<List<string>> Upload(ImagesUpload imagesUpload);
+        Task<List<string>> Upload(List<IFormFile> imagesUpload);
     }
 }
