e4fe9990d00e458d65cb48dddbf99caf73047a0a
quyentho <thokimquangquyen@gmail.com>
2021-06-18 09:10:51 +0700

    Revert "Change DateTime kind 2"
    This reverts commit b6082760225fb9539cfe5619a6910d9206347044.

--- MongoShop/MongoShop.BusinessDomain/Products/Product.cs
+++ MongoShop/MongoShop.BusinessDomain/Products/Product.cs
@@ -39,8 +39,6 @@ namespace MongoShop.BusinessDomain.Products
 
         [BsonElement("images")]
         public List<string> Images { get; set;}
-        
-        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
 
         public DateTime CreatedAt { get; set; }
 
--- MongoShop/MongoShop.BusinessDomain/Users/ApplicationUser.cs
+++ MongoShop/MongoShop.BusinessDomain/Users/ApplicationUser.cs
@@ -26,7 +26,6 @@ namespace MongoShop.BusinessDomain.Users
         public string Name { get; set; }
 
         [BsonElement("birthday")]
-        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTimeOffset BirthDay { get; set; }
 
         [BsonElement("status")]
