80362d79dced328e2c2d75bad751588cc108a55f
quyentho <thokimquangquyen@gmail.com>
2023-02-12 17:43:39 +0700

    Fix to run docker-compose

--- MongoShop/MongoShop.Server/Startup.cs
+++ MongoShop/MongoShop.Server/Startup.cs
@@ -129,7 +129,7 @@ namespace MongoShop.Server
                 c.RoutePrefix = string.Empty;
             });
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
